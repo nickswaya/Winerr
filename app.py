3e2458516faef8318335956c4654d55799f53ce2
Nick Swaya <nickswaya@gmail.com>
2024-08-10 16:44:42 -0500

    strip integration

--- app.py
+++ app.py
@@ -2,13 +2,20 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError
 from flask_bcrypt import Bcrypt
+import stripe
+from apscheduler.schedulers.background import BackgroundScheduler
 import logging
+from datetime import datetime, timedelta
+import pytz
 from logging import Formatter, FileHandler
 from forms import *
+from dotenv import load_dotenv
 import os
+load_dotenv()
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -17,8 +24,15 @@ import os
 app = Flask(__name__)
 app.config.from_object('config')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['STRIPE_TEST_PUBLIC_KEY'] = os.getenv("STRIPE_TEST_PUBLIC_KEY")
+app.config['STRIPE_TEST_SECRET_KEY'] = os.getenv("STRIPE_TEST_SECRET_KEY")
+stripe.api_key = app.config['STRIPE_TEST_SECRET_KEY']
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
+scheduler = BackgroundScheduler()
+scheduler.start()
 
 # Automatically tear down SQLAlchemy.
 '''
@@ -46,12 +60,15 @@ class User(db.Model):
     username = db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    current_score = db.Column(db.Integer, default=0)
+    total_spend = db.Column(db.Integer, default=0)
+    last_updated = db.Column(db.DateTime, default=datetime.now())
 
 
 # Define Transaction model
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
     g_bucks = db.Column(db.Integer(), unique=False, nullable=False)
     price = db.Column(db.Float(), unique=False, nullable=False)
 
@@ -63,11 +80,32 @@ class Transaction(db.Model):
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
+    top_users = db.session.query(User.username, db.func.sum(Transaction.price).label('total_price'))\
+    .order_by(db.func.sum(Transaction.price).desc())\
+     .limit(100)\
+     .all()
+    
+    top_users = db.session.query(
+        User.username, db.func.sum(Transaction.price).label('total_price')
+    ).join(Transaction, User.id == Transaction.user_id)\
+     .group_by(User.username)\
+     .order_by(db.func.sum(Transaction.price).desc())\
+     .limit(100)\
+     .all()
+    
+    winners = User.query.order_by(User.current_score.desc()).limit(100).all()
+    return render_template('pages/placeholder.home.html', top_users=top_users)
 
 
 @app.route('/about')
 def about():
+    # top_users = db.session.query(Transaction.price).all()
+    # top_users = db.session.query(
+    #     User.username, db.func.sum(Transaction.price).label('total_price')
+    # ).join(Transaction, User.username == Transaction.username)\
+    #  .group_by(User.username).all()
+     
+    # print(top_users)
     return render_template('pages/placeholder.about.html')
 
 
@@ -87,18 +125,54 @@ def yourrights():
     return render_template('pages/placeholder.yourrights.html')
 
 
+@app.route('/checkout', methods=['GET'])
+def checkout():
+    return render_template('forms/checkout.html', key=app.config['STRIPE_TEST_PUBLIC_KEY'])
+
+@app.route('/charge', methods=['POST'])
+def charge():
+    try:
+        amount = int(request.form['amount'])
+        token = request.form['stripeToken']
+
+        charge = stripe.Charge.create(
+            amount=amount,
+            currency='usd',
+            description='Example charge',
+            source=token
+        )
+        user = User.query.get(session['user_id'])
+        dollars = amount / 100
+        if user:
+            print(user)
+            user.total_spend += dollars
+            user.current_score += dollars
+            user.last_updated = datetime.now()
+        new_transaction = Transaction(user_id=user.id, g_bucks=dollars, price=dollars)
+        db.session.add(new_transaction)
+        db.session.commit()
+        user_transactions = Transaction.query.filter_by(user_id=user.id).all()
+        total_price = sum([transaction.price for transaction in user_transactions])
+        flash(f"Transaction of ${dollars} recorded! You've spent ${total_price}! Seek Therapy!", 'success')
+        return jsonify({'success': True})
+
+    except stripe.error.CardError as e:
+        return jsonify({'success': False, 'error': str(e)})
+    
+
 @app.route('/buy', methods=['GET', 'POST'])
 def buy():
     form = PayForm(request.form)
     user = User.query.get(session['user_id'])
+    # user = session.get(User, session['username'])
     if form.is_submitted():
         amount = request.form.get('amount')
-        new_transaction = Transaction(username=user.username, g_bucks=amount, price=amount)
+        new_transaction = Transaction(user_id=user.id, g_bucks=amount, price=amount)
         db.session.add(new_transaction)
         db.session.commit()
-        flash(f'Transaction of ${amount} recorded!', 'success')
-    else:
-        flash('No amount selected', 'danger')
+        user_transactions = Transaction.query.filter_by(user_id=user.id).all()
+        total_price = sum([transaction.price for transaction in user_transactions])
+        flash(f"Transaction of ${amount} recorded! You've spent ${total_price}! Seek Therapy!", 'success')
     return render_template('forms/buy.html', form=form)
 
 
@@ -106,33 +180,41 @@ def buy():
 def login():
     form = LoginForm(request.form)
     if form.is_submitted():
-         user = User.query.filter_by(username=form.username.data).first()
-         if user and bcrypt.check_password_hash(user.password, form.password.data):
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             session['user_id'] = user.id
+            session['username'] = form.username.data
+            session['user_logged_in']=True
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
-         else:
+        else:
             flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('forms/login.html', form=form)
 
 
 @app.route('/logout')
 def logout():
+    session['user_logged_in']=False
     session.pop('user_id', None)
     flash('You have been logged out!', 'success')
-    return redirect(url_for('login'))
+    return redirect(url_for('home'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm(request.form)
     if form.is_submitted():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created!', 'success')
-        return redirect(url_for('login'))
+        try:
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            user = User(username=form.username.data, email=form.email.data, password=hashed_password, total_spend=0, last_updated=None, current_score=0)
+            db.session.add(user)
+            db.session.commit()
+            flash('Your account has been created!', 'success')
+            return redirect(url_for('login'))
+        except IntegrityError:
+            db.session.rollback()
+            flash('Email or Username already exists. Please use a different email.', 'error')
+            return redirect(url_for('register'))
     return render_template('forms/register.html', form=form)
 
 
@@ -141,8 +223,10 @@ def forgot():
     form = ForgotForm(request.form)
     return render_template('forms/forgot.html', form=form)
 
-# Error handlers.
 
+
+
+# Error handlers.
 
 @app.errorhandler(500)
 def internal_error(error):
@@ -173,6 +257,27 @@ if not app.debug:
 def users():
     users = User.query.limit(10).all()
     return render_template('users.html', users=users)
+
+
+
+#----------------------------------------------------------------------------#
+# Degrade.
+#----------------------------------------------------------------------------#
+
+def degrade_scores():
+    with app.context():
+        now = datetime.now()
+        users = User.query.all()
+        for user in users:
+            time_diff = now - user.last_updated
+            hours = time_diff.total_seconds() / 3600
+            degrade_amount = int(hours)  # $1 per hour, convert to cents
+            if degrade_amount > 0:
+                user.current_score = max(user.current_score - degrade_amount, 0)
+                user.last_updated = now
+                db.session.commit()
+
+scheduler.add_job(degrade_scores, 'interval', hours=1)  # Run every hour
 
 
 #----------------------------------------------------------------------------#
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from datetime import datetime, timedelta
 from app import db, app
 
 
@@ -25,23 +26,30 @@ class User(Base):
     username = db.Column(db.String(120), unique=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(30))
+    total_spend = db.Column(db.Integer, default=0)
+    current_score = db.Column(db.Integer, default=0)
+    last_updated = db.Column(db.DateTime, default=datetime.now())
 
-    def __init__(self, username=None, password=None, email=None):
+
+    def __init__(self, username=None, password=None, email=None, current_score=None, last_updated=None, total_spend=None):
         self.username = username
         self.password = password
         self.email = email
+        self.total_spend=total_spend
+        self.current_score=current_score
+        self.last_updated=last_updated
 
 
 class Transaction(Base):
     __tablename__ = 'Transactions'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(120), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, unique=False, nullable=False)
     g_bucks = db.Column(db.Integer(), unique=False, nullable=False)
     price = db.Column(db.Float(), unique=False, nullable=False)
 
-    def __init__(self, username=None, g_bucks=None, price=None):
-        self.username = username
+    def __init__(self, user_id=None, g_bucks=None, price=None):
+        self.user_id = user_id
         self.g_bucks = g_bucks
         self.price = price
 
